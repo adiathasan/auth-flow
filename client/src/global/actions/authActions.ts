d459964515fe630e78b109e1a89231a064803d2e
adiathasan <adiathasan.me@gmail.com>
2021-05-02 00:27:34 +0600

    actions page comment added for readability

--- client/src/global/actions/authActions.ts
+++ client/src/global/actions/authActions.ts
@@ -1,1 +1,9 @@
-export const a = '';
+/*
+
+    []- Notice that I could have used actions before dispatching it.
+    []- Instead I dispatched directly as I'm using graphQl hooks where
+    []- I get more flexibiliity for dispatching. 
+
+*/
+
+export const action = () => null;
